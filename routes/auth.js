077b14d28a9d2b47da116f1f25366fdee0c1c59f
josephlarrivy <joseph.larrivy@gmail.com>
2023-05-16 17:41:10 -0500

    just need to finish profiles and applications

--- routes/auth.js
+++ routes/auth.js
@@ -34,6 +34,7 @@ router.post("/token", async function (req, res, next) {
 
     const { username, password } = req.body;
     const user = await User.authenticate(username, password);
+
     const token = createToken(user);
     return res.json({ token });
   } catch (err) {
@@ -61,6 +62,7 @@ router.post("/register", async function (req, res, next) {
 
     const newUser = await User.register({ ...req.body, isAdmin: false });
     const token = createToken(newUser);
+
     return res.status(201).json({ token });
   } catch (err) {
     return next(err);
